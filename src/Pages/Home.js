3e1824ec0b7611e58b9768ac30f33bd5f9ed401d
Shouhardik Saha <shouhardiksaha@gmail.com>
2022-01-08 15:37:20 +0530

    start

---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+    return ( 
+        <div className = "home" >
+        <h1>Search for Weather</h1>
+        <input type = "text"
+        placeholder = "City Name..." / >
+        <button >Search</button>
+        
+        </div>
+    );
+}
+
+export default Home
